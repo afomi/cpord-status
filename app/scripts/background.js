e742e7715ab922e52649e62744690046f4721506
Ryan Wold <wold@afomi.com>
2013-11-03 16:10:03 -0800

    add Bootstrap and JQuery, pass data from popup to Background page

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,44 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+// Chrome-Extension ENV
+
+var bg = function () {
+  return chrome.extension.getBackgroundPage();
+}
+
+// UI
+function toggleButtonsOff() {
+  $("button").removeClass("btn-primary");
+}
+
+// DATA
+var cpord = {
+  "status" : "idle",
+  "active" : "do"
+};
+
+function setStatus(status) {
+  cpord.status = status;
+}
+
+function getStatus(status) {
+  return cpord.status;
+}
+
+function updateState (id) {
+  bg().setStatus(id);
+}
+
+// INIT
+$(function () {
+  // when the Popup loads...
+  $("button").click(function() {
+    toggleButtonsOff();
+    $(this).addClass("btn-primary");
+
+    updateState($(this).attr('id'));
+  });
+
+  // Toggle the appropriate button when the popup loads
+  $("button#" + bg().getStatus()).addClass("btn-primary");
 });
